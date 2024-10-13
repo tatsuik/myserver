8127d6fefddb864d5d4621de9bd2d42e1992b7f2
Tatsuhiko Ikeda <i.tatsuh@me.com>
2024-10-13 14:20:48 +0000

    post, getを追加

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,72 @@
-use actix_web::{get, post, test, web, body, App, HttpResponse, HttpServer, Responder};
+use actix_web::{body, get, http::{self, header::{self, TryIntoHeaderPair}}, post, test, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Serialize, Deserialize};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, world!")
+#[derive(Debug, Serialize, Deserialize)]
+struct SensorData {
+    date: u32,
+    time: u32,
+    temperature: f32,
+    humidity: f32,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[get("/data")]
+async fn get_data() -> impl Responder {
+    HttpResponse::Ok().json(SensorData {
+        date: 20241012,
+        time: 210000,
+        temperature: 21.3,
+        humidity: 42.4
+    })
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[post("/data")]
+async fn post_data(data: web::Json<SensorData>) -> impl Responder {
+    println!{"{:?}", data};
+    HttpResponse::Ok().body("ok")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(post_data)
+            .service(get_data)
     })
-    .bind(("127.0.0.1", 8888))?
+    .bind(("0.0.0.0", 8888))?
     .run()
     .await
 }
 
 #[cfg(test)]
 #[actix_web::test]
-async fn test_hello() {
-    let app = test::init_service(App::new().service(hello)).await;
+async fn test_get_data() {
+    let app = test::init_service(App::new().service(get_data)).await;
 
-    let req = test::TestRequest::get().to_request();
+    let req = test::TestRequest::get().uri("/data").to_request();
     let resp = test::call_service(&app, req).await;
 
     assert!(resp.status().is_success());
-    let body = resp.into_body();
-    let bytes = body::to_bytes(body).await;
-    assert_eq!(bytes.unwrap(), web::Bytes::from_static(b"Hello, world!"))
+    let json : SensorData = test::read_body_json(resp).await;
+    assert_eq!(json.date, 20241012);
+    assert_eq!(json.time, 210000);
+    assert_eq!(json.temperature, 21.3);
+    assert_eq!(json.humidity, 42.4);
 }
 
+#[actix_web::test]
+async fn test_post_data() {
+    let mut app = test::init_service(App::new().service(post_data)).await;
+    let data = SensorData {
+        date: 20241013, 
+        time:225040, 
+        temperature:19.2, 
+        humidity:45.0
+    };
+    let res = test::TestRequest::post()
+        .uri("/data")
+        .append_header(header::ContentType::json())
+        .set_json(data)
+        .send_request(&mut app).await;
+
+    assert!(res.status().is_success());
+}
